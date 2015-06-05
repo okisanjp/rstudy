854c247578a5dc1b4fb066cd7d686b3d4076367c
okisanjp <okisan.jp@gmail.com>
2015-06-05 16:11:35 +0900

    ページタイトルを追加

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -69,6 +69,6 @@ class EntriesController < BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def entry_params
-      params.require(:entry).permit(:user_id, :url, :comment, :category)
+      params.require(:entry).permit(:user_id, :url, :title, :comment, :category)
     end
 end
